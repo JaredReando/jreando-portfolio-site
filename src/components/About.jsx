574eb315ff6098a79442d34849f74f1f7537140e
Jared Reando <jaredreando@gmail.com>
2019-02-15 13:07:57 -0800

    Add component structure diagram to README.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,6 +41,7 @@ function About(props) {
         />
       </div>
       <div id='about-content'>
+      <a href="https://www.apple.com/" target="_blank">apple-bapple-dot-rapple.com</a>
         <h2>About</h2>
         <p>On January 22, 2018, Austin, TX united in the first Hi, How Are You Day. Austinites were encouraged to check in on a neighbor, friend, co-worker, family member or loved one and ask, “Hi, How Are You?” - and to really mean it! Founders Tom Gimbel and Courtney Blanton were inspired by Austin’s love for the iconic friendly frog mural painted by Daniel Johnston - himself a world-renowned musician and visual artist despite his own struggles with mental health issues. The success of the day - and the outpouring of support from around the world - inspired the formation of the Hi, How Are You Project. The mission is to remove the stigma and shame around mental health, so people will feel open to communicate.
         <br/>
